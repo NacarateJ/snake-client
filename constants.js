57c9f341fc8af8616b6487d915081efda9af17cf
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-20 06:51:32 +0000

    Create module for constant values

---
+++ constants.js
@@ -0,0 +1,26 @@
+const IP = "localhost";
+const PORT = 50541;
+
+const NAME = "Name: JN"
+
+const MOVE_UP_KEY = "w"
+const MOVE_LEFT_KEY = "a"
+const MOVE_DOWN_KEY = "s"
+const MOVE_RIGHT_KEY = "d"
+
+const MESSAGES = {
+  g: "Say: Game over for you!",
+  h: "Say: Watch out for walls!",
+  j: "Say: Time to eat!",
+};
+
+module.exports = {
+  IP,
+  PORT,
+  NAME,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES,
+};
