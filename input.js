502ed0ed28e9f0ab8bdf818e151fa5557f8dc79f
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-20 06:55:16 +0000

    Use contants created in the handleUserInput() function

--- input.js
+++ input.js
@@ -1,3 +1,12 @@
+const {
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES,
+} = require("./constants");
+
+
 // Stores the active TCP connection object.
 let connection;
 
@@ -6,26 +15,22 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
   }
-  if (key === "w") {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === "a") {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === "s") {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === "d") {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
-  if (key === "g") {
-    connection.write("Say: Game over for you!");
-  }
-  if (key === "h") {
-    connection.write("Say: Watch out for walls!");
-  }
-  if (key === "j") {
-    connection.write("Say: Time to eat!");
+  for (const messageKey of Object.keys(MESSAGES)) {
+    if (key === messageKey) {
+      connection.write(MESSAGES[messageKey]);
+    }
   }
 };
 
