7e5e34036bf9a1c89084b9294e75ca477fbba3e6
Jaqueline <jaquelinenacarate@gmail.com>
2023-04-22 18:58:41 +0000

    Lint play.js

--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 // connect function sets up a connection and returns an object
 const { connect } = require("./client");
-// setup interface to handle user input from stdin 
+// setup interface to handle user input from stdin
 const { setupInput} = require("./input");
 
 
